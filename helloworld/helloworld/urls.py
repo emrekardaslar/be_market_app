cbf1d246a4f9df3aa592c98582ffd3bdd225c77a
emrekardaslar <emre.kardaslar@gmail.com>
2022-11-11 11:57:46 +0300

    add swagger support

--- helloworld/helloworld/urls.py
+++ helloworld/helloworld/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from quickstart import views
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -24,6 +26,11 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api_schema', get_schema_view(title='API Schema', description='Guide for the REST API'), name='api_schema'),
+    path('swagger_ui/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url': 'api_schema'}
+    ), name='swagger_ui'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
