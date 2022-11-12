a82299b5b9008d2bc42a3876ff33218a8d50b89e
emrekardaslar <emre.kardaslar@gmail.com>
2022-11-12 22:29:35 +0300

    add sample tests

--- helloworld/manager/tests.py
+++ helloworld/manager/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
 # Create your tests here.
+client = APIClient()
+
+
+class AuthTests(TestCase):
+
+    def test_login(self):
+        client.login(username='emre', password='emre')
+
+    def test_logout(self):
+        client.logout()
--- helloworld/product/tests.py
+++ helloworld/product/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductTests(TestCase):
+    def setUp(self):
+        Product.objects.create(
+            name="Selpak",
+            price=5.99,
+            imgLink="http://link",
+            category="Mendil",
+            subcategory="Mendil",
+            description="Selpak mendil"
+        )
+
+    def test_product(self):
+        selpak = Product.objects.get(name="Selpak")
+        self.assertEqual(selpak.price, 5.99)
