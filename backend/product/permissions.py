6c0b1273f265f738477020f4bb962a5bff17a50a
emrekardaslar <emre.kardaslar@gmail.com>
2022-11-19 17:50:47 +0300

    add custom permissions

---
+++ backend/product/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework import permissions
+
+
+class IsReadOnlyButStaff(permissions.BasePermission):
+    edit_methods = ("PUT", "PATCH", "POST", "DELETE")
+
+    def has_permission(self, request, view):
+        if request.user.is_staff or request.method not in self.edit_methods:
+            return True
+
+        return False
+
+
+class IsReadOnlyButUser(permissions.BasePermission):
+    edit_methods = ("PUT", "PATCH", "POST", "DELETE")
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated or request.method not in self.edit_methods:
+            return True
+
+        return False
--- backend/product/views.py
+++ backend/product/views.py
@@ -2,21 +2,25 @@ from rest_framework import viewsets, permissions
 from .models import Product, Comment, Rating, Order, OrderItem
 
 from .serializers import ProductSerializer, CommentSerializer, RatingSerializer, OrderSerializer, OrderItemSerializer
+from .permissions import IsReadOnlyButStaff, IsReadOnlyButUser
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('name')
     serializer_class = ProductSerializer
+    permission_classes = [IsReadOnlyButStaff]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('created_at')
     serializer_class = CommentSerializer
+    permission_classes = [IsReadOnlyButUser]
 
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all().order_by('created_at')
     serializer_class = RatingSerializer
+    permission_classes = [IsReadOnlyButUser]
 
 
 class OrderViewSet(viewsets.ModelViewSet):
