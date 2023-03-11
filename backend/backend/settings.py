9afb20f4bdbde3bfd5499984be0911136f1203ad
emrekardaslar <emre.kardaslar@gmail.com>
2023-03-11 20:15:04 +0300

    add dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,6 +100,18 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_NAME'),
+#         'USER': os.environ.get('POSTGRES_USER'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#         'HOST': "db",
+#         'PORT': 5432,
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
