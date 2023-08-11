7c84591a6850bd36e7bcf597ad949bae3b2fbf13
emrekardaslar <emre.kardaslar@gmail.com>
2023-08-11 23:10:26 +0300

    add models to admin panel

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Product, Comment
+from .models import Product, Comment, Order, OrderItem, Rating, User
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Comment)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Rating)
